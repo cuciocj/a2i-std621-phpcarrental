362601dda036a7f2c3ebfeaa42caa826cfa43f57
Christopher John Cucio <38850432+cuciocj@users.noreply.github.com>
2020-02-28 20:03:44 +1300

    update script

--- db_scripts/car_rental.sql
+++ db_scripts/car_rental.sql
@@ -1,62 +1,69 @@
-CREATE TABLE `users` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `username` varchar(20) UNIQUE NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `email` varchar(30) UNIQUE NOT NULL,
-  `date_joined` date NOT NULL,
-  `role_id` int NOT NULL
-);
-
 CREATE TABLE `roles` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar[15] UNIQUE NOT NULL
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `vehicles` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `body` varchar(15) NOT NULL,
   `color` varchar(10) NOT NULL,
   `transmission` varchar(10) NOT NULL,
   `image` varchar(255) NOT NULL,
-  `price` float NOT NULL DEFAULT 0
-);
+  `price` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-CREATE TABLE `transactions` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `user_id` int NOT NULL,
-  `vehicle_id` int NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
-  `approving_officer` int NOT NULL
-);
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `date_joined` date NOT NULL,
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `rent_requests` (
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
-  `vehicle_id` int NOT NULL
-);
+  `vehicle_id` int(11) NOT NULL,
+  UNIQUE KEY `rent_requests_UN` (`user_id`),
+  KEY `rent_requests_FK_vehicle_id` (`vehicle_id`),
+  CONSTRAINT `rent_requests_FK_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `rent_requests_FK_vehicle_id` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `vehicle_id` int(11) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `approving_officer` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transactions_FK_user_id` (`user_id`),
+  KEY `transactions_FK_vehicle_id` (`vehicle_id`),
+  KEY `transactions_FK_approving_officer` (`approving_officer`),
+  CONSTRAINT `transactions_FK_approving_officer` FOREIGN KEY (`approving_officer`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `transactions_FK_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `transactions_FK_vehicle_id` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `user_feedbacks` (
-  `user_id` int NOT NULL,
-  `subject` varchar(50) NOT NULL DEFAULT "no subject",
-  `message` varchar(255) NOT NULL DEFAULT "no message",
+  `user_id` int(11) DEFAULT NULL,
+  `subject` varchar(50) NOT NULL DEFAULT 'no subject',
+  `message` varchar(255) NOT NULL DEFAULT 'no message',
   `type` varchar(10) NOT NULL,
-  `date_received` date DEFAULT "now()"
-);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
-
-ALTER TABLE `transactions` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `transactions` ADD FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`);
-
-ALTER TABLE `transactions` ADD FOREIGN KEY (`approving_officer`) REFERENCES `users` (`id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `rent_requests` (`user_id`);
-
-ALTER TABLE `rent_requests` ADD FOREIGN KEY (`vehicle_id`) REFERENCES `vehicles` (`id`);
-
-ALTER TABLE `user_feedbacks` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  `date_received` date NOT NULL,
+  KEY `user_feedbacks_FK_user_id` (`user_id`),
+  CONSTRAINT `user_feedbacks_FK_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
