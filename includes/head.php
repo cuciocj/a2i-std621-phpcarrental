f831fb3f2443cb849073ea869799a3f8b4993917
Christopher John Cucio <cuciocj@gmail.com>
2020-03-03 18:54:54 +1300

    changed capitalization

--- includes/head.php
+++ includes/head.php
@@ -7,5 +7,5 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
 
-    <title>Racar - Rent A Car</title>
+    <title>RaCar - Rent a Car</title>
 </head>
