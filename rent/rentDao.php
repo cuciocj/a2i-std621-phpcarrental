38a2ac1b7970333e40281ce93ae5806dbe5959fb
Christopher John Cucio <cuciocj@gmail.com>
2020-03-05 00:30:44 +1300

    :tada: initiali commit

---
+++ rent/rentDao.php
@@ -0,0 +1,42 @@
+<?php
+
+    class RentDao {
+        
+        private $table = "rent_requests";
+
+        private $db;
+
+        public function __construct() {
+            $this->db = new DB();
+        }
+
+        public function insert($rent) {
+            $flag = false;
+
+            $sql = "insert into " . $this->table
+                . '(user_id, start_date, end_date, vehicle_id) values'
+                . '(?, ?, ?, ?);';
+
+            $con = $this->db->getConnection();
+            $stmt = $con->prepare($sql);
+            $stmt->bind_param("issi", $p_userId, $p_startDate, $p_endDate, $p_vehicleId);
+
+            $p_userId = $rent->getUser();
+            $p_startDate = $rent->getStartDate();
+            $p_endDate = $rent->getEndDate();
+            $p_vehicleId = $rent->getVehicle();
+
+            if($stmt->execute() === true) {
+               $flag = true;
+            } else {
+                echo "couldn't execute sql: " . $con->error;
+            }
+
+            $stmt->close();
+            $con->close();
+            return $flag;
+        }
+        
+    }
+
+?>
