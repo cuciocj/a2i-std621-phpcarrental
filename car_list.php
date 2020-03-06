b53e9801d6a91f7461fe2db4e2aae6ca93a16139
Christopher John Cucio <cuciocj@gmail.com>
2020-03-06 21:28:12 +1300

    added car edit modal functionality

--- car_list.php
+++ car_list.php
@@ -34,6 +34,60 @@ $vehicles = $vehicleDao->list();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <script>
+        $(document).ready(function() {
+            var carInfo;
+
+            $('#editCarModal').on('show.bs.modal', function (event) {
+                var button = $(event.relatedTarget);
+                carInfo = button.data('info');
+                console.log(carInfo);
+                var modal = $(this);
+                modal.find('#name').attr('value', carInfo.name);
+                modal.find('#body').attr('value', carInfo.body);
+                modal.find('#color').attr('value', carInfo.color);
+                modal.find('#transmission').attr('value', carInfo.transmission);
+                modal.find('#image_url').attr('value', carInfo.image);
+                modal.find('#price').attr('value', carInfo.price);
+                modal.find(carInfo.isReserved == 1 ? '#status_reserved' :
+                             '#status_available').prop('checked', true);     
+            });
+
+            $('#btn_save').on('click', function() {
+                // console.log(carInfo.id);
+                var car_name = $('#name').val();
+                var car_body = $('#body').val();
+                var car_color = $('#color').val();
+                var car_transmission = $('#transmission').val();
+                var car_image = $('#image_url').val();
+                var car_price = $('#price').val();
+                var car_isreserved = $('input[name="car_availability_option"]:checked').val();
+
+                $.post('./vehicle/vehicleController.php',
+                    {
+                        carId: carInfo.id,
+                        carName: car_name,
+                        carBody: car_body,
+                        carColor: car_color,
+                        carTransmission: car_transmission,
+                        carImage: car_image,
+                        carPrice: car_price,
+                        carIsReserved: car_isreserved
+                    },
+                    function(data, status, jqXHR) {
+                        if(data == 'success') {
+                            alert('Car details modified successfully!');
+                            $('#editCarModal').modal('hide');
+                            location.reload();
+                        } else {
+                            alert('Edit failed. Please try again.');
+                        }
+                    });
+            });
+        });
+
+    </script>
 </head>
 
 <body>
@@ -70,7 +124,8 @@ $vehicles = $vehicleDao->list();
                                 <td><?= $vehicle->price; ?></td>
                                 <td><?= ($vehicle->isReserved == 1 ? 'reserved' : 'available'); ?></td>
                                 <td>
-                                    <button type="button" class="btn btn-primary btn-sm btn-block" data-toggle="modal" data-target="#editCarModal">Edit</button>
+                                    <button type="button" class="btn btn-primary btn-sm btn-block" 
+                                        data-toggle="modal" data-target="#editCarModal" data-info='<?= json_encode($vehicle); ?>'>Edit</button>
                                     <button type="button" class="btn btn-secondary btn-sm btn-block">Remove</button>
                                 </td>
                             </tr>
@@ -82,7 +137,6 @@ $vehicles = $vehicleDao->list();
     </div>
 
     <!-- Edit Car Modal -->
-    <!-- Modal -->
     <div class="modal fade" id="editCarModal" tabindex="-1" role="dialog" aria-labelledby="editCarModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
             <div class="modal-content">
@@ -119,21 +173,21 @@ $vehicles = $vehicleDao->list();
                             <input type="text" id="price" name="price" value="">
                         </div>
                         <div>
-                            <label for="price"><strong>Status</strong></label>
+                            <label for="car_availability_option"><strong>Status</strong></label>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="available">
-                                <label class="form-check-label" for="inlineRadio1">available</label>
+                                <input class="form-check-input" type="radio" name="car_availability_option" id="status_available" value="0">
+                                <label class="form-check-label" for="status_available">available</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="reserved">
-                                <label class="form-check-label" for="inlineRadio2">reserved</label>
+                                <input class="form-check-input" type="radio" name="car_availability_option" id="status_reserved" value="1">
+                                <label class="form-check-label" for="status_reserved">reserved</label>
                             </div>
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button id="btn_save" type="button" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
         </div>
