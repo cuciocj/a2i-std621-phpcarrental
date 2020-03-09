7c4c1d0b1b4bb0e33c797609ceb1e9e0701de447
Christopher John Cucio <cuciocj@gmail.com>
2020-03-10 11:40:46 +1300

    :tada: initial commit; implemented insert function

---
+++ transaction/transactionDao.php
@@ -0,0 +1,40 @@
+<?php
+
+    class TransactionDao {
+
+        private $table = "transactions";
+
+        private $db;
+
+        public function __construct() {
+            $this->db = new DB();
+        }
+
+        public function insert($transaction) {
+            $flag = false;
+            $sql = "insert into " . $this->table 
+                    . " (user_id, vehicle_id, start_date, end_date, approving_officer)"
+                    . " values (?, ?, ?, ?, ?);";
+
+            $con = $this->db->getConnection();
+            $stmt = $con->prepare($sql);
+            $stmt->bind_param("iissi", $p_userid, $p_vehicleid, $p_startdate, $p_enddate, $p_officer);
+            $p_userid = $transaction->getUser();
+            $p_vehicleid = $transaction->getVehicle();
+            $p_startdate = $transaction->getStartDate();
+            $p_enddate = $transaction->getEndDate();
+            $p_officer = $transaction->getApprovingOfficer();
+
+            if($stmt->execute() == true) {
+                $flag = true;
+            } else {
+                echo "error sql:" . $con->error;
+            }
+
+            $stmt->close();
+            $con->close();
+            return $flag;
+        }
+    }
+
+?>
