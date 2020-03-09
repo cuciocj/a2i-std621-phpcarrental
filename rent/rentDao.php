95c83e49a1daf171c90cbae90054ccd6e968a4c5
Christopher John Cucio <cuciocj@gmail.com>
2020-03-09 15:23:55 +1300

    implemented list function

--- rent/rentDao.php
+++ rent/rentDao.php
@@ -10,6 +10,47 @@
             $this->db = new DB();
         }
 
+        public function list() {
+            $sql = "select u.id u_id, u.username, u.name u_name, u.email," 
+                . " v.id v_id, v.name v_name, v.image, r.start_date, r.end_date"
+                . " from rent_requests r"
+                . " join users u on r.user_id = u.id"
+                . " join vehicles v on r.vehicle_id = v.id;";
+
+                $con = $this->db->getConnection();
+                $list = array();
+
+                if($result = $con->query($sql)) {
+                    if($result->num_rows > 0) {
+                        while ($row = $result->fetch_array()) {
+                            $user = new User();
+                            $user->setId($row['u_id']);
+                            $user->setUsername($row['username']);
+                            $user->setName($row['u_name']);
+                            $user->setEmail($row['email']);
+
+                            $vehicle = new Vehicle();
+                            $vehicle->setId($row['v_id']);
+                            $vehicle->setName($row['v_name']);
+                            $vehicle->setImage($row['image']);
+                            
+                            $rent = new Rent();
+                            $rent->setStartDate($row['start_date']);
+                            $rent->setEndDate($row['end_date']);
+                            $rent->setUser($user);
+                            $rent->setVehicle($vehicle);
+
+                            array_push($list, $rent);
+                        }
+                    }
+                } else {
+                    echo "rentDao: couldn't execute sql: " . $sql . " error: " . $con->error;
+                }
+                $con->close();
+
+            return $list;
+        }
+
         public function insert($rent) {
             $flag = false;
 
