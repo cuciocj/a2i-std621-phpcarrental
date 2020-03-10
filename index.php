594352d5859956770933ec18d734f171125a581a
Christopher John Cucio <cuciocj@gmail.com>
2020-03-10 19:08:06 +1300

    removed unnecessary comments

--- index.php
+++ index.php
@@ -43,8 +43,6 @@ $vehicles = $vehicleDao->list();
             $('#carModal').on('show.bs.modal', function(event) {
                 var button = $(event.relatedTarget); // Button that triggered the modal
                 carInfo = button.data('info'); // Extract info from data-* attributes
-                // // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-                // // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
                 console.log(carInfo);
                 carId = carInfo.id;
                 var modal = $(this);
