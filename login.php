1e6b688f09c5ebf395964b8c69686ef0734a0530
sagarpokhrel <34935128+sagarpokhrel@users.noreply.github.com>
2020-03-13 14:09:20 +1300

    header and footer / forgot password
    header and footer design fixed and added forgot password.

--- login.php
+++ login.php
@@ -55,12 +55,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include './includes/head.php'; ?>
+   <?php include './includes/head.php'; ?>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script src="js/popper.min.js"></script>
+    <link rel="stylesheet" href="https://jqueryui.com/resources/demos/style.css">
+    <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
+    <link rel="stylesheet" type="text/css" href="css/nav.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    
+    
 </head>
 
 <body>
@@ -88,11 +92,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <div class="g-recaptcha" data-sitekey="6LcMLt4UAAAAAM8mkcVtez61P8hCQ4dxYqwBiOxl"></div>
                         <br />
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <div class="row">
+                        <div class="col-4">
+                            <button type="submit" class="btn btn-primary">Sign In</button>
+                        </div>
+                        <div class="col-4"></div>
+                        <div class="col-4">
+                            <a href="forgot_pw.php">Forgot Password?</a>
+                        </div>
+                    </div>
+                    
                 </form>
             </div>
         </div>
     </div>
     <?php include './includes/footer.php'; ?>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/datepicker.js"></script>
+    <script src="js/nav.js"> </script>
+    <script src="js/popper.min.js"></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </body>
 </html>
