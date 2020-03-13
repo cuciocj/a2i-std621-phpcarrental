0e452b100bcfdeca4d59119322d56541cb44f10c
Christopher John Cucio <cuciocj@gmail.com>
2020-03-13 22:17:00 +1300

    :sparkles: :makeup: added navbar

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-// todo if staff goes to index.php, must redirect where he belongs
 
 include_once './commons/db.php';
 include_once './vehicle/vehicle.php';
@@ -30,16 +29,12 @@ $vehicles = $vehicleDao->list();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://jqueryui.com/resources/demos/style.css">
     <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
-    <link rel="stylesheet" type="text/css" href="css/nav.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
 
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> -->
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <script type="text/javascript" src="js/datepicker.js"></script>
-    <script src="js/nav.js"> </script>
-    <script src="js/popper.min.js"></script>
 
     <script>
         $(document).ready(function() {
