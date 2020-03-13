cd755d9b77e12f8fdf139aa56902706338d6e370
Christopher John Cucio <cuciocj@gmail.com>
2020-03-13 13:59:09 +1300

    added checking if user is enabled for find function; implemented list, update and delete function

--- user/userDao.php
+++ user/userDao.php
@@ -12,7 +12,7 @@
 
         public function find($user) {
             $sql = "select * from " . $this->table 
-                . " where username = ? and password = ?;";
+                . " where username = ? and password = ? and is_enabled = 1;";
 
             $con = $this->db->getConnection();
             $stmt = $con->prepare($sql);
@@ -33,6 +33,7 @@
                         $user->setEmail($row['email']);
                         $user->setDateJoined($row['date_joined']);
                         $user->setRole($row['role_id']);
+                        $user->setEnabled($row['is_enabled']);
                     }
                 } else {
                     //echo "cannot find user [" . $user->getUsername() . "]";
@@ -48,15 +49,87 @@
 
             return $user;
         }
+
+        public function list() {
+            $sql = "select * from " . $this->table . ";";
+            $list = array();
+
+            $con = $this->db->getConnection();
+
+            if($result = $con->query($sql)) {
+                if($result->num_rows > 0) {
+                    while($row = $result->fetch_array()) {
+                        $user = new User();
+                        $user->setId($row['id']);
+                        $user->setUsername($row['username']);
+                        $user->setPassword($row['password']);
+                        $user->setName($row['name']);
+                        $user->setEmail($row['email']);
+                        $user->setDateJoined($row['date_joined']);
+                        $user->setRole($row['role_id']);
+                        $user->setEnabled($row['is_enabled']);
+                        array_push($list, $user);
+                    }
+                }
+            } else {
+                echo "userDao: couldn't execute sql: " . $sql . " error: " . $con->error;
+            }
+            $con->close();
+
+            return $list;
+        }
+
+        public function update($user) {
+            $flag = false;
+
+            $sql = "update " . $this->table
+                . " set username = ?, name = ?, email = ?, is_enabled = ? where id = ?;";
+
+            $con = $this->db->getConnection();
+            $stmt = $con->prepare($sql);
+            $stmt->bind_param("sssii",
+                $p_username,
+                $p_name,
+                $p_email,
+                $p_isEnabled,
+                $p_id
+            );
+
+            $p_username = $user->getUsername();
+            $p_name = $user->getName();
+            $p_email = $user->getEmail();
+            $p_isEnabled = $user->isEnabled();
+            $p_id = $user->getId();
+
+            if($stmt->execute()) {
+                $flag = true;
+            }
+
+            $stmt->close();
+            $con->close();
+            return $flag;
+        }
+
+        public function delete($user) {
+            $flag = false;
+
+            $sql = "delete from " . $this->table
+                . " where id = ?;";
+            
+            $con = $this->db->getConnection();
+            $stmt = $con->prepare($sql);
+            $stmt->bind_param("i", $p_id);
+
+            $p_id = $user->getId();
+            
+            if($stmt->execute() === true) {
+                $flag = true;
+            }
+
+            $stmt->close();
+            $con->close();
+            return $flag;
+        }
     }
 
-    // $user = new User();
-    // $user->setUsername("qweasd");
-    // $user->setPassword("qwe123");
-    // $userDao = new UserDao();
-    // $user = $userDao->find($user);
-
-    // echo $user->getId() . "|" . $user->getUsername() . "|" . $user->getPassword() . "|"
-    //     . $user->getName() . "|" . $user->getEmail() . "|" . $user->getDateJoined() . "|" . $user->getRole();
-    
 ?>
