29e47211430c832b31df51dd1d008763c857a866
Christopher John Cucio <cuciocj@gmail.com>
2020-03-13 13:58:31 +1300

    :tada: initial commit; impelemented update and delete user

---
+++ user/userController.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+
+if (!(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)) {
+    header("location: login.php");
+    exit;
+}
+
+include_once '../commons/db.php';
+include_once 'user.php';
+include_once 'userDao.php';
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $user;
+    $userDao;
+
+    if ($_POST['mode'] == 'update') {
+        $user = new User();
+        $user->setId(trim($_POST["id"]));
+        $user->setUsername(trim($_POST["username"]));
+        $user->setName(trim($_POST["name"]));
+        $user->setEmail(trim($_POST["email"]));
+        $user->setEnabled(trim($_POST["is_enabled"]));
+
+        $userDao = new UserDao();
+        if($userDao->update($user)) {
+            echo 'success';
+        } else {
+            echo 'fail';
+        }
+
+    } else if ($_POST['mode'] == 'add') {
+
+    } else if ($_POST['mode'] == 'delete') {
+        $user = new User();
+        $user->setId(trim($_POST["id"]));
+        $userDao = new UserDao();
+        if($userDao->delete($user)) {
+            echo 'success';
+        } else {
+            echo 'fail';
+        }
+    }
+    
+}
+
+?>
