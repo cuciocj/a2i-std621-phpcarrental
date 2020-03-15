bb132c9214a45cd66786925f491b7087e536a946
sagarpokhrel <34935128+sagarpokhrel@users.noreply.github.com>
2020-03-15 15:20:30 +1300

    forgot pw and register
    added design

--- forgot_pw.php
+++ forgot_pw.php
@@ -10,11 +10,75 @@
     <link rel="stylesheet" type="text/css" href="css/nav.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
+    <script src="~/assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.1/jquery.validate.min.js"></script>
+
     
 	
 </head>
 <body>
 	<?php include './includes/header.php'; ?>
+    <br><br><br><br>
+    <div class="PopUpBG">
+             <div class="PopUp">
+                <h3 class="modal-title">
+              <span>Reset Password</span>
+                </h3>
+                <div class="container">
+                <div class="row">
+                    <div class="col-3"></div>
+                    <div class="col-6">
+
+              <form id="form">
+                <p>Please enter your old password</p>
+
+              <input type="password" name="oldPw" id="oldPw" required="required" />
+              <br><br>
+
+              <p>Please enter your email address to reset your password</p>
+              <input type="email" name="ResetEmail" id="ResetEmail" placeholder="Email Address" required="required" />
+              <br><br>
+
+              <input type="button" class="btn btn-success" value="Send" onclick="ResetPassword(this);"/>
+              <br><br>
+
+            </form>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+            </div>
+            
+          </div>
+
+    </div>
+    <br><br>
+
+<script type="text/javascript">
+   function ResetPassword(e) {
+    if (!$("#form").validate())
+        return false;
+
+    $.ajax({
+        type: "POST",
+        url: "/Account/loginRequestResetPassword",
+        dataType: "json",
+        data: {
+            Email: $("#ResetEmail").val().trim(),
+        },
+        success: function () {
+            console.log("send");
+            $(".PopUp").html("We have sent mail to you");
+            setTimeout(function () {
+                $(".PopUpBG").fadeOut();
+            }, 10000); // fadeout the message after a few seconds
+        },
+        error: function () {
+            console.log('something went wrong - debug it!');
+        }
+    })
+} 
+
+</script> 
 
 	 <?php include './includes/footer.php'; ?>
 	 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
